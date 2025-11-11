ab1aab96bde05f1cd83ef7b425f582b73dd84f06
Mayel <mayelndiayeinfo@gmail.com>
2025-11-11 12:10:16 +0100

    added p5 and example of popup setup

---
+++ games/constructing/complete-triangulation.js
@@ -0,0 +1,9 @@
+function setup() {
+    const popup = document.getElementById('completetriangpopup');
+    const canvas = createCanvas(400, 400);
+    canvas.parent(popup);
+}
+
+function draw() {
+    background(220);
+}
